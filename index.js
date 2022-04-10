d4844c8f07f07847fdb92a6cb01d232cf30eb735
seungdang123 <seungdang123@gmail.com>
2022-04-10 15:36:17 +0900

    Login page

--- index.js
+++ index.js
@@ -1,65 +1,58 @@
 const emailInput = document.querySelector(".email--input");
-const passwordInput = document.querySelector(".pw--input");
-
+const emailLabel = document.querySelector(".email--label");
+const passwordInput = document.querySelector(".password--input");
+const passwordLabel = document.querySelector(".password--label");
 const loginButton = document.querySelector(".login--button");
 
-const emailLabel = document.querySelector(".label--email");
-const passwordLabel = document.querySelector(".label--password");
+let email = "";
+let password = "";
 
-const body = document.getElementsByTagName("body");
-
-const EmailClick = () => {
-  if (emailInput.value == "") {
-    if (emailLabel.classList.contains("label--on--finished")) {
-      emailLabel.classList.remove("label--on--finished");
-    }
-
-    if (emailInput.placeholder == "") {
-      emailLabel.classList.remove("label--on");
-    }
-    emailLabel.classList.add("label--on");
-    emailInput.placeholder = "";
-  }
-};
-
-const PasswordClick = () => {
-  if (passwordInput.value == "") {
-    if (passwordLabel.classList.contains("label--on--finished")) {
-      passwordLabel.classList.remove("label--on--finished");
-    }
-    passwordLabel.classList.add("label--on");
-    passwordInput.placeholder = "";
-  }
-};
-
-emailInput.addEventListener("click", EmailClick);
-passwordInput.addEventListener("click", PasswordClick);
-
-const checkForm = () => {
-  if (emailInput.value != "" && passwordInput.value != "") {
+const ButtonOn = () => {
+  if (email != "" && password != "") {
     loginButton.classList.add("login--button--on");
+  } else {
+    loginButton.classList.remove("login--button--on");
   }
+};
 
-  if (emailInput.value != "") {
+emailInput.onfocus = () => {
+  console.log("Focus");
+  emailLabel.classList.remove("label--on--finished");
+  emailLabel.classList.add("label--on");
+  emailInput.placeholder = "";
+};
+
+emailInput.onblur = () => {
+  if (emailInput.value == "") {
+    emailLabel.classList.remove("label--on");
+    emailInput.placeholder = "이메일";
+  } else if (emailInput.value != "") {
     emailLabel.classList.replace("label--on", "label--on--finished");
   }
+};
 
-  if (passwordInput.value != "") {
-    passwordLabel.classList.replace("label--on", "label--on--finished");
-  }
+passwordInput.onfocus = () => {
+  console.log("Focus");
+  passwordLabel.classList.remove("label--on--finished");
+  passwordLabel.classList.add("label--on");
+  passwordInput.placeholder = "";
+};
 
-  if (emailInput.value == "") {
-    emailLabel.classList.remove("label--on--finished");
-    emailInput.placeholder = "이메일";
-    loginButton.classList.remove("login--button--on");
-  }
-
+passwordInput.onblur = () => {
   if (passwordInput.value == "") {
-    passwordLabel.classList.remove("label--on--finished");
+    passwordLabel.classList.remove("label--on");
     passwordInput.placeholder = "비밀번호";
-    loginButton.classList.remove("login--button--on");
+  } else if (passwordInput.value != "") {
+    passwordLabel.classList.replace("label--on", "label--on--finished");
   }
 };
 
-emailInput.addEventListener("change", checkForm);
-passwordInput.addEventListener("change", checkForm);
+emailInput.onkeyup = () => {
+  email = emailInput.value;
+  ButtonOn();
+};
+
+passwordInput.onkeyup = () => {
+  password = passwordInput.value;
+  ButtonOn();
+};
